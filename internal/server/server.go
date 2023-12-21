ae03a4a175ae27d8cfb7661f195fcefb42c6e126
Дмитрий Мельник <dmitrijmelnik@MacBook-Pro-Dmitrij.local>
2023-12-21 18:07:09 +0300

    iter6 - server:Run fix

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,8 +2,6 @@ package server
 
 import (
 	"context"
-	"errors"
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
@@ -28,22 +26,12 @@ func (s *Server) Run() error {
 	serverCtx, serverStopCtx := context.WithCancel(context.Background())
 
 	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGTSTP)
 	go func() {
 		<-sig
-
-		shutdownCtx, _ := context.WithTimeout(serverCtx, 30*time.Second)
-
-		go func() {
-			<-shutdownCtx.Done()
-			if errors.Is(shutdownCtx.Err(), context.DeadlineExceeded) {
-				log.Fatal("graceful shutdown timed out.. forcing exit.")
-			}
-		}()
-
-		err := s.app.Shutdown(shutdownCtx)
+		err := s.app.Shutdown(serverCtx)
 		if err != nil {
-			log.Fatal(err)
+			panic(err)
 		}
 		serverStopCtx()
 	}()
