a4d109892a06cef1af571f8ef39322202afaa5ca
Дмитрий Мельник <dmitrijmelnik@MacBook-Pro-Dmitrij.local>
2024-01-25 18:51:36 +0300

    iter13 - memory  repo retriable

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -39,7 +39,6 @@ func run() error {
 
 	// TODO: если обработать тесты упадут
 	connPG, _ := db.NewPsqlConnection(c)
-	connPG = nil
 	if connPG != nil {
 		defer connPG.Close()
 		if err := migrateDatabase(
